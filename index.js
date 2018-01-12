ace6a6e87492dcf8f00a4c8b0ffd635befdf5403
Victor Borov <vitos123@gmail.com>
2018-01-12 06:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase()){
     return "I can\'t hear you!"
-  } else if(string.toUpperCase()){
+  } else if(string.toUpperCase() === uppercase){
     return "YES INDEED!"
   }else if(string === "I love you, too."){
     return "I love you, too."
